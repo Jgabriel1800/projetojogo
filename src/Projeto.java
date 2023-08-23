b3cc8befc7b8953f559ba6a2a8ee076c058629e7
João Gabriel <jgabriel.1800@hotmail.com>
2023-08-22 22:51:53 -0300

    Miniprojeto7completo

--- src/Projeto.java
+++ src/Projeto.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 public class Projeto {
 public static void main(String[] args) {
         
-        Leitor leitor= new Leitor();
+        Leitordearquivo leitor= new Leitordearquivo();
         Scanner escaneador = new Scanner(System.in, "UTF-8");
         
-        Map<String, personagem> personagens = leitor.lerpersonagens("rsc/Personagens.txt");
-        Map<String,capitulo>capitulos=leitor.lerCapitulos("rsc/Capitulos.txt",personagens,escaneador);
+        Map<String, personagem> personagens = leitor.carregarpersonagens("rsc/personagem.txt");
+        Map<String,capitulo>capitulos=leitor.carregarCapitulos("rsc/capitulo.txt",personagens,escaneador);
+        
         System.out.println("Carregamento finalizado\n\n...\n\n");
-        capitulo raiz = capitulos.get("1");
+        
+        capitulo raiz = capitulos.get("capitulo1");
 
         raiz.executar();
         
        escaneador.close();
     }
 }
+
--- src/capitulo.java
+++ src/capitulo.java
@@ -1,38 +1,46 @@
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
+
 public class capitulo {
     private String texto;
-    protected ArrayList<escolha> escolhas;
+    private ArrayList<escolha> escolhas;
     private personagem jogador;
     private int habilidade;
-    protected Scanner escaneador;
+    private Scanner escaneador;
 
     protected capitulo() {
         this.texto = "";
         this.escolhas = new ArrayList<escolha>();
     }
+
     public capitulo(String texto, personagem jogador, int habilidade, Scanner escaneador)  {
         this.texto=texto;
         this.jogador=jogador;
         this.habilidade=habilidade;
         this.escaneador=escaneador;
         this.escolhas=new ArrayList<escolha>();
+    
     }
+
     public capitulo(Map<String, personagem> personagens, Scanner escaneadordoconsole,Scanner escaneadordearquivo) 
     {
-        this.lercapitulo(personagens,escaneadordearquivo);
+        this.LerCapitulo(personagens,escaneadordearquivo);
         this.escaneador=escaneadordoconsole;
         this.escolhas=new ArrayList<escolha>();
     }
-    protected void lercapitulo(Map<String,personagem> personagens, Scanner escaneadordearquivo){ 
+    
+    protected void LerCapitulo(Map<String,personagem> personagens, Scanner escaneadordearquivo){ 
+        
         escaneadordearquivo.nextLine();
         String idjogador = escaneadordearquivo.nextLine();
         this.jogador = personagens.get(idjogador);
+
         escaneadordearquivo.nextLine();
         String linha = escaneadordearquivo.nextLine();
         this.texto = "";
         while (!linha.equals("VARIACOESHabilidade")) {
+            
             if (linha.equals(idjogador)) {
                 texto = texto + jogador.getNome();
             } else {
@@ -42,48 +50,54 @@ public class capitulo {
         }
         this.habilidade = Integer.parseInt(escaneadordearquivo.nextLine());
     }
-    public void adicionarEscolha(escolha escolha) {
+    public void adicionarescolha(escolha escolha) {
         escolhas.add(escolha);
     }
     public void executar() {
         mostrar();
+
         if (escolhas.size() > 0) {
-            int idCapituloEscolhido = escolher();
+            int idcapituloescolhido = escolher();
             System.out.println();
             System.out.println(". . .");
             System.out.println();
-            escolhas.get(idCapituloEscolhido).getProximo().executar();
+            escolhas.get(idcapituloescolhido).getProximo().executar();
         } else {
             System.out.println("FIM");
         }
     }
+    
     protected void mostrar() {
         System.out.println(texto);
         jogador.ajustarhabilidade(habilidade);
+
         for (int i = 0; i < escolhas.size(); i++) {
             System.out.println("- " + escolhas.get(i).getTextoMostrado());
         }
+        System.out.println(">>");
     }
-    protected int escolher() {
-        int opcaoEscolhida = 0;
+    
+    public int escolher() {
+        int opcaoescolhida = 0;
         String escolha;
         boolean escolhavalida = false;
+
         while (!escolhavalida) {
+
         escolha = escaneador.nextLine();
         for (int i = 0; i < escolhas.size(); i++) {
             if (escolha.equalsIgnoreCase(escolhas.get(i).getTextoDigitado())) {
                 escolhavalida = true;
-                opcaoEscolhida = i;
+                opcaoescolhida = i;
             }
         }
         if (!escolhavalida) {
             System.out.println("Escolha inválida");
         }
     }
-    return opcaoEscolhida;
+    return opcaoescolhida;
+  }
 }
-}
-    
 
 
 
